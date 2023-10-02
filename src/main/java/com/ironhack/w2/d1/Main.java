5e7bfa3535e087dde21e70cb7a327f5d08cfcb50
Adrián Maza <adri_mv1@hotmail.com>
2023-10-02 10:12:30 +0200

    Maven and testing examples added

---
+++ src/main/java/com/ironhack/w2/d1/Main.java
@@ -0,0 +1,27 @@
+package com.ironhack.w2.d1;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie movie1 = new Movie("A Clockwork Orange", 130);
+        Movie movie2 = new Movie("The Shining", 120);
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+        Director director = new Director("Stanley", "Kubrick", movieList);
+
+        System.out.println(director);
+
+
+//        GSON
+
+        Gson gson = new Gson();
+        String directorJSON = gson.toJson(director);
+
+        System.out.println(directorJSON);
+    }
+}
